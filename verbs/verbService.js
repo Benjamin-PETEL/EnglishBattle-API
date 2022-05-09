1d05ac8090c3e82044b90eb93ab32e2376105b23
Benjamin PETEL <benjamin.petel@outlook.com>
2022-05-09 08:45:14 +0200

    Feature: add verbs - get

--- verbs/verbService.js
+++ verbs/verbService.js
@@ -22,7 +22,11 @@ const _CSVToArray = (data, delimiter = ',', omitFirstRow = false) => {
         .split('\n')
         .map(str => {
             const verbArray = str.split(delimiter);
+<<<<<<< HEAD
             return { bv: verbArray[0], sp: verbArray[3], pp: verbArray[4] };
+=======
+            return { baseVerbal: verbArray[0], simplePast: verbArray[3], pastParticipe: verbArray[4] };
+>>>>>>> fcb3ca3 (Feature: add verbs - get)
         });
 };
 
--- verbs/verbService.test.js
+++ verbs/verbService.test.js
@@ -4,7 +4,11 @@ describe('getVerbs', () => {
     test('nominalCase', () => {
         verbService.initVerbs();
         const verbs = verbService.getVerbs();
+<<<<<<< HEAD
         const expected = [{ bv: 'go', sp: 'went', pp: 'gone' }];
+=======
+        const expected = [{ baseVerbal: 'go', simplePast: 'went', pastParticipe: 'gone' }];
+>>>>>>> fcb3ca3 (Feature: add verbs - get)
         expect(verbs).toBeDefined();
         expect(Array.isArray(verbs)).toBe(true);
         expect(verbs).toEqual(expect.arrayContaining(expected));
