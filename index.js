10b03b75f3f195649ff3d2a3eaa9941b8c60ee3f
PETEL <benjamin.petel@capgemini.com>
2022-07-29 11:52:52 +0200

    Bug: add a default port to the server

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { initVerbs } = require('./verbs/verbService');
 initVerbs();
 
 const app = require('./server.js');
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log('express running on port ' + port);
